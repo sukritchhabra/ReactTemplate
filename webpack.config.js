94d1b6df00083a3e345370f3451bbc2521483c9d
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-10-12 05:50:55 -0400

    Adding HtmlWebpackPlugin and ExtractTextPlugin.
    * HtmlWebpackPlugin is generating the new Html everytime a build occurs.
    * ExtractTextPlugin is extracting the css parsed in the loader to a
    different file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Copy assets to /dist
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+    },
 
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader"
+          })
+        }
+      ]
     },
 
     plugins: [
         new CopyWebpackPlugin([ {from:'./src/assets/images',to:'assets/images'} ]),
+        new HtmlWebpackPlugin({ title: 'Output Management' }),
+        new ExtractTextPlugin("styles.css"),
     ]
 };
