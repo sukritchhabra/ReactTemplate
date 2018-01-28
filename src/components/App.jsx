51fe3b17e40a8ebfff88ee8ab689edb4a521cb32
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2018-01-28 17:09:55 -0500

    Creating a basic redux connected counter component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import { Route, Link } from 'react-router-dom';
 
 import Header from 'components/Header';
+import Counter from 'components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
         <Header />
+        <Counter />
       </div>
     );
   }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { sampleIncrementAction, sampleDecrementAction } from 'state/sample';
+import 'styles/Counter.scss';
+
+// A basic counter component connected to the redux store.
+class Counter extends Component {
+  render() {
+    return (
+      <div className="counter">
+        <h1 className="value">{this.props.value}</h1>
+        <button className="inc-button" onClick={this.props.incValue}>Increment</button>
+        <button className="dec-button" onClick={this.props.decValue}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  incValue: PropTypes.func.isRequired,
+  decValue: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    value: state.sample.sampleValue,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    incValue: () => dispatch(sampleIncrementAction()),
+    decValue: () => dispatch(sampleDecrementAction()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
