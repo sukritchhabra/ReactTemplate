45671f1366f35b8aa47cbd34a46eea65bfd10e5b
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2018-01-28 18:46:56 -0500

    Removing Counter component from App.jsx.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-// import { Route, Link } from 'react-router-dom';
-
 import Header from 'components/Header';
-import Counter from 'components/Counter';
+// import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
         <Header />
-        <Counter />
       </div>
     );
   }
