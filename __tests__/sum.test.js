44aecdfac137e4c58f01724b0bb8e4c925784554
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2018-05-28 03:24:05 -0400

    Fixing other line endings and lint issues

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,7 +1,7 @@
-import { Sum } from '../src/components/Header.jsx';
+import { Sum } from '../src/components/Header';
 
 describe('Testing Sum', () => {
-    test('Expect 1 + 3 to be 4', () => {
-        expect(Sum(1, 3)).toBe(4);
-    });
+  test('Expect 1 + 3 to be 4', () => {
+    expect(Sum(1, 3)).toBe(4);
+  });
 });
